bd7fc95b1b3c50284e5c39c5fee7c2296fe20cc4
Alexander Sauna <alexandersauna05@gmail.com>
2024-06-14 12:25:40 -0500

    Creè el repositorio

---
+++ P_inventario/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P_inventario.DB;
+
+public partial class Categoria
+{
+    public int Id { get; set; }
+
+    public string? NombreCategoria { get; set; }
+
+    public virtual ICollection<Elemento> Elementos { get; set; } = new List<Elemento>();
+}
---
+++ P_inventario/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P_inventario.DB;
+
+public partial class Role
+{
+    public int Id { get; set; }
+
+    public string NombreRol { get; set; } = null!;
+
+    public virtual ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+}
